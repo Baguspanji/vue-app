0e8f774071ee177d4764a789d71cb251bcc0509e
baguspanji <pitungs060@gmail.com>
2021-12-22 14:14:10 +0700

    config

--- src/config.js
+++ src/config.js
@@ -3,4 +3,4 @@ import axios from 'axios'
 const auth = JSON.parse(window.localStorage.getItem('lbUser'))
 
 axios.defaults.baseURL = 'http://147.139.193.105/resik/v1/'
-axios.defaults.headers.common['Authorization'] = auth.token
+axios.defaults.headers.common['Authorization'] = auth ? auth.token : ''
